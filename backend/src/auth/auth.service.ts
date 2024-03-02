d9f81459807ee246983b3d16d1da5b3ccc4506fd
Sergey Kochetov <anashkagonit@gmail.com>
2024-03-02 17:24:06 +0300

    Донастроа стартового шаблона Nest

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,26 +1,16 @@
 import { Injectable } from '@nestjs/common'
-import { CreateAuthDto } from './dto/create-auth.dto'
-import { UpdateAuthDto } from './dto/update-auth.dto'
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from 'src/user/user.service';
+import { AuthDto } from './dto/auth.dto';
 
 @Injectable()
 export class AuthService {
-	create(createAuthDto: CreateAuthDto) {
-		return 'This action adds a new auth'
-	}
+  constructor (
+    private jwt: JwtService,
+    private userService: UserService
+  ) {}
 
-	findAll() {
-		return `This action returns all auth`
-	}
-
-	findOne(id: number) {
-		return `This action returns a #${id} auth`
-	}
-
-	update(id: number, updateAuthDto: UpdateAuthDto) {
-		return `This action updates a #${id} auth`
-	}
-
-	remove(id: number) {
-		return `This action removes a #${id} auth`
-	}
+  async login(dto:AuthDto) {
+    return dto
+  }
 }
